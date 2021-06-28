d53c33395f39847652d0b0a54d91880c8daa84c1
Dorian Edwards <de.pharmd2018@gmail.com>
2021-06-27 22:01:45 -0400

    add proposed models for database

---
+++ models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ['admin', 'mentor', 'student'],
+  },
+  // Courses and steps are listed here to allow us to search mentors or students by course or steps
+  courses: [{ type: Schema.Types.ObjectId, ref: 'Course' }],
+  steps: [{ type: Schema.Types.ObjectId, ref: 'Step' }],
+  availability: [Date],
+  // nested channel object since a channel is just two strings.
+  // The alternative is a subdocument of two strings, or a platform name string and an array of users and their contact info.
+  channels: [
+    {
+      platform: {
+        type: String,
+        required: true,
+      },
+      address: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  // collaborators for a student user would be mentors assisting (High-Def Student Login Frame 33)
+  // for mentors it would be students helped (High-Def Mentor Sign-in Flow Frame 32)
+  collaborators: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+})
+
+module.exports = mongoose.model('User', userSchema)
