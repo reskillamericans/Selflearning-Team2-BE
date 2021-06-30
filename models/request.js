b3c8e291c6d4f349d993ae83164bf35d62e66762
Dorian Edwards <de.pharmd2018@gmail.com>
2021-06-30 15:07:51 -0400

    make pending the default status

--- models/request.js
+++ models/request.js
@@ -5,7 +5,7 @@ const requestSchema = new Schema(
   {
     step: { type: Schema.Types.ObjectId, ref: 'Step' },
     student: { type: Schema.Types.ObjectId, ref: 'User' },
-    status: { type: String, enum: ['pending', 'accepted', 'closed'] },
+    status: { type: String, enum: ['pending', 'accepted', 'closed'], default: 'pending' },
     mentor: { type: Schema.Types.ObjectId, ref: 'User' },
   },
   { timestamps: true }
