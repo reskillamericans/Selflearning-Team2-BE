88b3be86a6bc6be400c006c0dda64d20802c9cde
Dorian Edwards <de.pharmd2018@gmail.com>
2021-06-30 14:11:29 -0400

    clean up user model

--- models/user.js
+++ models/user.js
@@ -14,18 +14,7 @@ const userSchema = new Schema({
   courses: [{ type: Schema.Types.ObjectId, ref: 'Course' }],
   steps: [{ type: Schema.Types.ObjectId, ref: 'Step' }],
   availability: [Date],
-  // nested channel object since a channel is just two strings.
-  // The alternative is a subdocument of two strings, or a platform name string and an array of users and their contact info.
-  channels: [
-    {
-      platform: {
-        type: String,
-      },
-      address: {
-        type: String,
-      },
-    },
-  ],
+  channels: [{platform: String, address: String}]
 })
 
 module.exports = mongoose.model('User', userSchema)
